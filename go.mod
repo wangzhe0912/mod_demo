19d48c6a832681617f1f1090511ff4bfcd6dd39e
wangzhe0912 <wangzhe0912@tju.edu.cn>
2021-08-23 17:53:11 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/wangzhe0912/mod_demo
+
+go 1.16
---
+++ mod_demo.go
@@ -0,0 +1,9 @@
+package mod_demo
+
+import (
+    "fmt"
+)
+
+func Hi(name string) string {
+    return fmt.Sprintf("Hi, %s", name)
+}
